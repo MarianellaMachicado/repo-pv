42736e8da90600dcdbec4cc1ae2845d7082a1857
MarianellaMachicado <machicadomarianella@gmail.com>
2022-08-03 15:04:30 -0300

    Se sube proyecto base para el tp final

---
+++ Tp-Final-Machicado4044/src/main/java/ar/edu/unju/fi/pvisual/TpFinalMachicado4044Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalMachicado4044Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalMachicado4044Application.class, args);
+	}
+
+}
